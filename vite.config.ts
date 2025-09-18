7981b22235f25ab41f22900d8b357c2e65703ab4
Yasir Shaukat <yasirzinbox@gmail.com>
2025-09-18 19:27:34 +0500

    Vite path set to show app on customer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'   // if you use React
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // 👇 This is the key line to make assets load correctly through /apps/customizer
+  base: './',
+
   plugins: [
+    react(),        // include if you are using React
     tailwindcss(),
   ],
 })
