b6f6c2fa87d17b0736c749f8bf1c72ade5115c05
Yasir Shaukat <yasirzinbox@gmail.com>
2025-09-23 21:04:31 +0500

    Change background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 // import KonvaImage from "./components/konva-image";
+// import KonvaImage from "./components/konva-image";
 import ProductCustomizer from "./features/product-customizer";
 // import ProductCustomizer from "./features/product-customizer";
 
 export default function App() {
   return (
     <>
-    <div className="bg-gray-400">
-      <ProductCustomizer />
+      <div className="bg-gray-300">
+        <div className="max-w-7xl mx-auto">
+          <ProductCustomizer />
+        </div>
       </div>
       {/* <KonvaImage /> */}
     </>
